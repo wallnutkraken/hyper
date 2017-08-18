6639a6a9469b0ffe41cc6d89b1c580c2beddbd53
Emile <emil73d6@edu.eal.dk>
2017-08-18 22:27:41 +0200

    added redirect

--- hyper/main.go
+++ hyper/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/urfave/cli"
+	"os"
+)
+
 func main() {
-	
+	/* Initialize the cli */
+	hyperApp := cli.NewApp()
+	hyperApp.Name = "hyper"
+	hyperApp.Usage = "hyper [command] [arguments]"
+	hyperApp.Version = version
+
+	/* Set up the commands */
+	hyperApp.Commands = []cli.Command {
+	}
+
+	hyperApp.Run(os.Args)
 }
